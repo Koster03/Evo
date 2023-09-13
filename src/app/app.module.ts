6f76a7ecb1f8ccc3dc3e923488679c9b39cfd259
Koster03 <pyatkin-kostik@mail.ru>
2023-09-13 15:52:02 +0300

    Directives&Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { PlusFivePipe } from './components/pipes/plus-five.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    PlusFivePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pipes/plus-five.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'plusFive',
+})
+export class PlusFivePipe implements PipeTransform {
+  transform(value: number, ...args: unknown[]): number {
+    return value + 5;
+  }
+}
